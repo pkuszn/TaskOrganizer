bd3b56e6e4d503436ec8d4b3a8e465aeaff8f7fb
patryk <patryk.kuszneruk1798@gmail.com>
2022-03-02 14:36:59 +0100

    Created second dispatcherTimer which runs on the another thread

--- TaskOrganizer/ViewModel/PomodoroViewModel.cs
+++ TaskOrganizer/ViewModel/PomodoroViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Input;
 using System.Windows.Threading;
 using TaskOrganizer.Model;
@@ -11,19 +13,7 @@ namespace TaskOrganizer.ViewModel
     {
         private string _currentTime;
         private string _currentDate;
-
-        public int InputTime { get; set; }
-        public ICommand StartCountingCommand { get; set; }
-        
-        public DispatcherTimer Time { get; set; }
-        public string outputMinutes { get; set; }
-      
-
-        public PomodoroViewModel()
-        {
-            DateText();
-            StartCountingCommand = new RelayCommand(UpdateTime);
-        }
+        private int _currentPomodoroTick;
 
         public string CurrentTime
         {
@@ -53,23 +43,75 @@ namespace TaskOrganizer.ViewModel
             }
         }
 
+        public int CurrentPomodoroTick
+        {
+            get
+            {
+                return _currentPomodoroTick;
+            }
+            set
+            {
+                if (_currentPomodoroTick != value)
+                    _currentPomodoroTick = value;
+                OnPropertyChanged(nameof(_currentPomodoroTick));
+            }
+        }
+
+
+        public DispatcherTimer Time { get; set; }
+        public DispatcherTimer PomodoroTimer { get; set; }
+        public ICommand StartCountingCommand { get; set; }
+
+
+
+
+        public PomodoroViewModel()
+        {
+            DateText();
+            UpdateTime();
+            StartCountingCommand = new RelayCommand(StartPomodoroTimer);
+        }
+
         private void UpdateTime()
         {
 
-            Time = new DispatcherTimer();
-            Time.Interval = TimeSpan.FromSeconds(1);
+            Time = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
             Time.Tick += new EventHandler(TimeText);
             Time.Start();
         }
+    
 
-        private void TimeText(object sdender, EventArgs e)
+    private void TimeText(object sdender, EventArgs e)
         {
-            CurrentTime = DateTime.Now.ToString("HH:mm");
+            CurrentTime = DateTime.Now.ToString("HH:mm:ss");
         }
 
         private void DateText()
         {
             CurrentDate = DateTime.Now.ToString("dddd dd MMMM yyyy");
         }
+
+        private void StartPomodoroTimer()
+        {
+            PomodoroTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            PomodoroTimer.Tick += (s, e) => Task.Run(() => PomodoroTick(s, e));
+            PomodoroTimer.Start();
+        }
+
+        private void PomodoroTick(object sdender, EventArgs e)
+        {
+            CurrentPomodoroTick -= 1;
+            if (CurrentPomodoroTick == 0)
+                PomodoroTimer.Stop();
+            OnPropertyChanged(nameof(CurrentPomodoroTick));
+        }
+ 
     }
+   
 }
