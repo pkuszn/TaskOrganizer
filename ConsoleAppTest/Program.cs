6e1335520b13bde4d6c8bb2071c8a22e9884efd4
patryk <patryk.kuszneruk1798@gmail.com>
2022-04-09 13:37:22 +0200

    triggerr

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using TaskOrganizer.Domain.Models;
+using TaskOrganizer.Domain.Services;
+using TaskOrganizer.EFCore;
+using TaskOrganizer.EFCore.Services;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataService<TaskModel> taskService = new GenericDataService<TaskModel>(new MyDbContextFactory());
+            taskService.Create(new TaskModel
+            {
+                Id = 1,
+                TaskDesc = "dupa",
+                CreatedDate = DateTime.Now,
+                DoneTaskDate = DateTime.Now,
+                IsSelected = true
+            }); 
+        }
+    }
+}
---
+++ TaskOrganizer.EFCore/Migrations/20220409091912_next.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskOrganizer.EFCore.Migrations
+{
+    public partial class next : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSelected",
+                table: "PomodoroModels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSelected",
+                table: "PomodoroModels");
+        }
+    }
+}
